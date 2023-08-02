552ecdaeaec9fe81c6a392e77a50983b87dbcf9e
Chakrimaidam <m.chakri906@gmail.com>
2023-08-02 11:41:49 +0530

    Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import TodoList from "./TodoList";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [task, setTask] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  const changeHandler = (e) => {
+    setTask(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newTodo = [...todo, task];
+    setTodo(newTodo);
+    setTask("");
+  
+  };
+  const deleteHandler = (indexValue) =>{
+    const newTodo = todo.filter((todo,index) => index !== indexValue);
+    setTodo(newTodo);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo Mangement</h1>
+      <form onSubmit={submitHandler} className="header">
+        <input
+        className="text"
+          type="text"
+          name="task"
+          value={task}
+          onChange={changeHandler}
+        />
+        <input type="submit" name="Add" value="Add" />
+      </form>
+      <TodoList className="list" todolist={todo} deleteHandler={deleteHandler} />
     </div>
   );
-}
+};
 
 export default App;
